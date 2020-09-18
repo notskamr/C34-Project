894b7ea1a31789f4852e8bf87dee0c2270f85713
notskamr <66314478+notskamr@users.noreply.github.com>
2020-09-18 18:14:25 +0530

    3

--- sketch.js
+++ sketch.js
@@ -1,88 +1,59 @@
-var database ,dog,dog1,dog2
-var position
-//var form
-var feed,add
-var foodobject
-var Feedtime
-var Lastfeed
-//Create variables here
+var  dog, happyDog;
+
+var database;
+var foodS, foodStock;
 
 function preload()
-
 {
-  dogimg1 = loadImage("images/dogImg.png")
-  dogimg2 = loadImage("images/dogImg1.png")
+  dogImg = loadImage("images/dogImg.png");
+  dogImg1 = loadImage("images/dogImg1.png");
 	//load images here
 }
 
 function setup() {
-	createCanvas(1000, 500);
-  database = firebase.database();
-  console.log(database);
- 
-  foodobject=new Food()
-  dog = createSprite(550,250,10,10);
-  dog.addImage(dogimg1)
-  dog.scale=0.2
- 
-  var dogo = database.ref('Food');
-  dogo.on("value", readPosition, showError);
-  feed = createButton("FEED DRAGO")
-  feed.position(500,15)
-  feed.mousePressed(FeedDog)
-  add = createButton("ADD FOOD")
-  add.position(400,15)
-  add.mousePressed(AddFood)
-
-} 
-
-function draw(){
- background(46,139,87);
-
- foodobject.display()
- 
- drawSprites();
+  createCanvas(500,500);
   
- fill(255,255,254);
- textSize(15);
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale = 0.4;
+  
+  database = firebase.database();
 
-drawSprites();
-}
-function readPosition(data){
-  position = data.val();
-  foodobject.updateFoodStock(position)
-}
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
 
-function showError(){
-  console.log("cool error");
+    
 }
 
-function writePosition(nazo){
-  if(nazo>0){
-    nazo=nazo-1
+
+function draw() {  
+
+  background(46, 139, 87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
   }
-  else{
-    nazo=0
+
+  drawSprites();
+  //add styles here
+  strokeWeight()
+ 
+fill('red')
+  text("Food Remaining:" + foodS, 340,480);
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock (x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
   }
-  database.ref('/').set({
-    'Food': nazo
+
+  database.ref('/').update({
+    Food: x 
   })
-
-}
-function AddFood(){
-position++
-database.ref('/').update({
-  Food:position
-}
-
-)
-}
-function FeedDog(){
-
-dog.addImage(dogimg2)
-foodobject.updateFoodStock(foodobject.getFoodStock()-1)
- database.ref('/').update({
-   Food:foodobject.getFoodStock(),
-   FeedTime:hour ()
- })
 }
